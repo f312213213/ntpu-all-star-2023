a911151070ba829a6569aafc46b52f988d757b1a
david <f312213213david@gmail.com>
2023-03-28 23:06:33 +0800

    feat: dynamic show scroll to top button

--- vote/components/ScrollToTop/index.tsx
+++ vote/components/ScrollToTop/index.tsx
@@ -2,6 +2,7 @@ import { FaArrowUp } from 'react-icons/fa'
 import { sendGALog } from '@/vote/features/app/services'
 import { useAppDispatch } from '@/vote/features/store'
 import { useRouter } from 'next/router'
+import ScrollToTop from 'react-scroll-up'
 import useIsMobile from '@/vote/hooks/useIsMobile'
 
 const ScrollToTopButton = () => {
@@ -12,21 +13,29 @@ const ScrollToTopButton = () => {
     dispatch(sendGALog({
       eventName: 'scroll-to-top',
     }))
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    })
   }
 
   if (!router.pathname.includes('sport')) return null
 
   return (
-    <button title={'scroll-to-top'} className={'btn btn-circle btn-md fixed right-4 bottom-4'} style={{ marginBottom: isMobile ? '64px' : 0 }} onClick={onClick}>
+    <ScrollToTop
+      showUnder={200}
+      style={{
+
+        transitionDuration: '0.5s',
+      }}
+    >
+      <button
+        title={'scroll-to-top'}
+        className={'btn btn-circle btn-md fixed right-4 bottom-4'}
+        style={{ marginBottom: isMobile ? '64px' : 0 }}
+        onClick={onClick}
+      >
       <span className={'text-2xl'}>
         <FaArrowUp />
       </span>
-    </button>
+      </button>
+    </ScrollToTop>
   )
 }
 
